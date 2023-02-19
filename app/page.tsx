76e4e94c54eda430a9454bf8a1b52766fc037410
Asada678 <asada.tomoya0923@gmail.com>
2023-02-19 14:55:44 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import CreatePost from "./components/CreatePost";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-lg py-5">Hello next</h1>
+      <CreatePost />
+    </main>
+  );
+}
