4ce0f8e50354b2686703840438b11fa2df198385
Asada678 <asada.tomoya0923@gmail.com>
2023-02-19 18:43:45 +0900

    get post

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
 "use client";
+import axios from "axios";
 import CreatePost from "./components/CreatePost";
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Post";
+
+// Fetch all posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({ queryFn: allPosts, queryKey: ["posts"] });
+  if (error) return error;
+  if (isLoading) return "Loading...";
+  console.log("data:", data);
   return (
     <main>
-      <h1 className="text-lg py-5">Hello next</h1>
       <CreatePost />
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          name={post.user?.name}
+          avatar={post.user?.image}
+          postTitle={post.title}
+          id={post.id}
+        />
+      ))}
     </main>
   );
 }
